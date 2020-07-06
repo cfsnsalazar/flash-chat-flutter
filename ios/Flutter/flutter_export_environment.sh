16f96163f046ecb8290777866673466bf7ba2e24
Napoleon Salazar <nsalazar@coolfiresolutions.com>
2020-07-06 01:36:27 -0500

    ahhh

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/nsalazar/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nsalazar/Documents/projects/flash-chat-flutter"
-export "FLUTTER_TARGET=/Users/nsalazar/Documents/projects/flash-chat-flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/nsalazar/Documents/projects/flash-chat-flutter-forked"
+export "FLUTTER_TARGET=/Users/nsalazar/Documents/projects/flash-chat-flutter-forked/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
